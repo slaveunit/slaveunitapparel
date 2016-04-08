d1646f7a6f8f3a7e3c54d21d11613cdc8ed83a49
slaveunit <marco.abundo@live.com>
2016-04-08 02:03:08 +0000

    Attempt fix heroku routing error.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,47 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :products
+ 
+  get 'order_items/create'
+
+  get 'order_items/update'
+
+  get 'order_items/destroy'
+
+  get 'carts/show'
+
+#COMMENTS
+  resources :products do
+  resources :comments
+  end
+#COMMENTS
+  devise_for :users
+  resources :users 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
+# SHOPPING CART
+  resource :cart, only: [:show]
+  resources :order_items, only: [:create, :update, :destroy]
+# SHOPPING CART
+
+  get 'static_pages/store'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#landing_page'
+
+  # EMAIL
+  post 'static_pages/thank_you'
+
+  # PAYMENTS
+  post 'payments/create'
+  get 'payments/create'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,7 +50,8 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  # 
+  #   resources :products
+
   # Example resource route with options:
   #   resources :products do
   #     member do
@@ -52,5 +89,6 @@ Rails.application.routes.draw do
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #     #   end
+  #     resources :products
+  #   end
 end
